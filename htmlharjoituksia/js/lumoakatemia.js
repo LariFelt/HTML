0172ef69c582cf2e26fb7544b8b7943de87aceb8
unknown <lari.felt@edu.keuda.fi>
2023-09-15 11:31:32 +0300

    perjantai

---
+++ htmlharjoituksia/js/lumoakatemia.js
@@ -0,0 +1,12 @@
+console.log("Hei maailma");
+var num1, num2, summa;
+num1 = 7
+num2 = 5
+summa = num1 + num2
+console.log(summa);
+var num3, num4, erotus;
+num3 = 12
+num4 = 11
+erotus = num3 - num4
+console.log(erotus);
+
